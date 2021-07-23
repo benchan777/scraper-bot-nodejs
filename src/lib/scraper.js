2e3b43353b75e81c6acdee7d73ca1b3b80986946
benchan777 <benchan777@gmail.com>
2021-07-23 02:53:29 -0700

    add function to scrape tartine's country loaf bread stock

---
+++ src/lib/scraper.js
@@ -0,0 +1,48 @@
+const axios = require('axios').default;
+const cheerio = require('cheerio');
+const url = 'https://guerrero.tartine.menu/pickup/'
+
+const countryLoafScraper = (countryLoafStock) => {
+    return new Promise( (resolve, reject) => {
+        axios({
+            method: 'get',
+            url: url,
+        })
+            .then(response => {
+                const $ = cheerio.load(response.data);
+                const data = $.html();
+                let tempString = ''
+                const stockArray = []
+                for (let i = 0; i < data.length; i ++) {
+                    tempString += data[i]
+                    if (tempString.includes('"in_stock": true') === true) {
+                        stockArray.push('Available')
+                        tempString = ''
+                    }
+                    if (tempString.includes('"in_stock": false') === true) {
+                        stockArray.push('Not Available')
+                        tempString = ''
+                    }
+                }
+
+                if (stockArray[29] != countryLoafStock) {
+                    if (stockArray[29] == 'Available') {
+                        resolve(stockArray[29])
+                    } else if (stockArray[29] == 'Not Available') {
+                        resolve(stockArray[29])
+                    } else {
+                        reject('Failed to scrape')
+                    }
+                } else {
+                    resolve(stockArray[29])
+                }
+            })
+            .catch(error => {
+                reject(error.message)
+            })
+    })
+}
+
+module.exports = {
+    countryLoafScraper: countryLoafScraper,
+}
