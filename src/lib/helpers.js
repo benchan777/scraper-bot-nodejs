33cf1cd5dc848fde15c4800dbca1d1ccef583806
benchan777 <benchan777@gmail.com>
2021-08-17 15:16:33 -0700

    update messageEmbed helper to handle new stock status format

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -18,16 +18,18 @@ const messageEmbed = (stock) => {
     return new Promise( (resolve, reject) => {
         let color = ''
 
-        if (stock == 'Available') {
+        if (stock == true) {
+            availability = 'Available'
             color = '#00ff00'
         } else {
+            availability = 'Not Available'
             color = '#ff0000'
         }
         const newEmbed = new Discord.MessageEmbed()
             .setColor(color)
             .setTitle('Country Loaf')
             .setThumbnail('https://s3.amazonaws.com/toasttab/restaurants/restaurant-13508000000000000/menu/items/8/item-200000007632874878_1598045606.jpg')
-            .addField('Availability', stock, true)
+            .addField('Availability', availability, true)
         
         resolve(newEmbed)
     })
